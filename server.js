af1a1cfaeab0d34dc46648bf0959827828d01382
James S <jamests1000@gmail.com>
2023-01-19 23:04:41 -0500

    uncommented delete function

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ app.post('/api/notes', (req, res) => {
   res.json('succesfully saved');
 });
 
-// app.delete('/api/notes/:id', (req, res) => {
-//   const data = fs.readFileSync('./db/db.json', 'utf8');
-//   const notes = JSON.parse(data).filter(note => note.id !== req.params.id);
-//   const stringifyedNotes = JSON.stringify(notes, null, 2);
-//   fs.writeFileSync('./db/db.json', stringifyedNotes);
-//   res.json('note deleted');
-// });
+app.delete('/api/notes/:id', (req, res) => {
+  const data = fs.readFileSync('./db/db.json', 'utf8');
+  const notes = JSON.parse(data).filter(note => note.id !== req.params.id);
+  const stringifyedNotes = JSON.stringify(notes, null, 2);
+  fs.writeFileSync('./db/db.json', stringifyedNotes);
+  res.json('note deleted');
+});
 
 app.listen(PORT, () => console.log('running at port 3001'));
